3208e16a80e2f3ce00e53908aed7c2124ece0ac4
kennethachan <kennethandrechan@gmail.com>
2022-10-24 14:11:59 -0400

    word mapping WIP

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import "./App.css"
-import { useState, react } from "react"
+import { useState, react, useRef } from "react"
+
+const getCloud = () =>
+  `testing hello world random words this is a test`
+    .split(" ")
+    .sort(() => (Math.random() > 0.5 ? 1 : -1))
 
 function App() {
   const [userInput, setUserInput] = useState("")
+  const [activeWordIndex, setActiveIndex] = useState(0)
+
+  const cloud = useRef(getCloud())
 
   return (
     <div className="App">
       <h1>WPM Typing Test</h1>
+
+      <p>
+        {cloud.current.map((word, index) => {
+          if (index === activeWordIndex) {
+            return <b>{word} </b>
+          }
+
+          return <span>{word} </span>
+        })}
+      </p>
 
       <input
         type="text"
