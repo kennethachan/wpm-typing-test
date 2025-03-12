c9a02fcf82465ec93a39a85a89ca0f27625bc8ce
kennethachan <kennethandrechan@gmail.com>
2025-03-12 18:16:45 -0400

    commented out result metrics for future update

--- src/pages/Results.js
+++ src/pages/Results.js
@@ -14,9 +14,9 @@ const wpm = location.state?.wpm || 0; // Get WPM from navigation state
   <h1 className="results-title" onClick={() => navigate("/")} >The Typing Dojo</h1>
     <div className="results">
     <p className="wpm-result"><b>Your WPM:</b> {wpm}</p>
-    <p><b>Accuracy:</b> placeholder</p>
+    {/* <p><b>Accuracy:</b> placeholder</p>
     <p><b>Longest Letter Streak:</b> placeholder</p>
-    <p><b>Total Mistakes:</b> placeholder</p>
+    <p><b>Total Mistakes:</b> placeholder</p> */}
     <button className="try-again" onClick={() => navigate("/")}>Try Again</button>
     </div>
 
